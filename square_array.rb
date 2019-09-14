3ed9e81080ae20204194dbd6c4f31a32199c568f
Plimon Howard V <plimon.howard@gmail.com>
2019-09-14 18:11:20 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  arry_num
+  rt_arry = Array.new
+  while arry_num != array.length
+      rt_arry[arry_num] << array[arry_num] ** 2
+      arry_num++
+
+  return rt_array
 end
